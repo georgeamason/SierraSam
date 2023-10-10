a2d2f9684491f94c0e9d9c1c19934012ec3d5fec
George Mason <george.mason@red-gate.com>
2023-10-10 10:00:03 +0100

    Add EmptySerializer

---
+++ SierraSam.Core.Tests.Unit/Serializers/EmptySerializerTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using SierraSam.Core.Enums;
+using Spectre.Console;
+
+namespace SierraSam.Core.Tests.Unit.Serializers;
+
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+internal sealed class EmptySerializerTests
+{
+    private readonly Core.Serializers.EmptySerializer _serializer = new();
+
+    [Test]
+    public void Serialize_returns_expected_response()
+    {
+        var content = new TerseMigration(
+            MigrationType.Versioned,
+            "1",
+            "someDescription",
+            "someType",
+            "someChecksum",
+            DateTime.UtcNow,
+            MigrationState.Pending);
+
+        var result = _serializer.Serialize(content);
+
+        result.Should().BeEquivalentTo(Text.Empty);
+    }
+}
---
+++ SierraSam.Core/Serializers/EmptySerializer.cs
@@ -0,0 +1,9 @@
+﻿using Spectre.Console;
+using Spectre.Console.Rendering;
+
+namespace SierraSam.Core.Serializers;
+
+internal sealed class EmptySerializer : ISerializer
+{
+    public IRenderable Serialize<T>(T content) => new Text(string.Empty);
+}

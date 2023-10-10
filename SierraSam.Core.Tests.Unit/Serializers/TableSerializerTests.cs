9f4879b49f9d4397dd8bb07eb62dafec3443875d
George Mason <george.mason@red-gate.com>
2023-10-10 09:59:50 +0100

    Add TableSerializer

---
+++ SierraSam.Core.Tests.Unit/Serializers/TableSerializerTests.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using SierraSam.Core.Serializers;
+using Spectre.Console.Testing;
+
+namespace SierraSam.Core.Tests.Unit.Serializers;
+
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+internal sealed class TableSerializerTests
+{
+    private readonly TableSerializer _serializer = new();
+    private readonly TestConsole _console = new ();
+
+    private record ExampleForTest(string Forename, string Surname);
+
+    [Test]
+    public void Serialize_returns_expected_response()
+    {
+        var content = new ExampleForTest[]
+        {
+            new ("Joe", "Blogs"),
+            new ("Karen", "Smith")
+        };
+
+        var result = _serializer.Serialize(content);
+
+        _console.Write(result);
+
+        const string expected =
+            """
+                                  
+            | Forename | Surname |
+            | :------- | :------ |
+            | Joe      | Blogs   |
+            | Karen    | Smith   |
+                                  
+            
+            """;
+
+        _console.Output.Should().Be(expected.NormalizeLineEndings());
+    }
+
+    [Test]
+    public void Serialize_returns_expected_response_for_non_array()
+    {
+        var content = new ExampleForTest("Joe", "Blogs");
+
+        var result = _serializer.Serialize(content);
+
+        _console.Write(result);
+
+        const string expected =
+            """
+                                  
+            | Forename | Surname |
+            | :------- | :------ |
+            | Joe      | Blogs   |
+                                  
+            
+            """;
+
+        _console.Output.Should().Be(expected.NormalizeLineEndings());
+    }
+}
---
+++ SierraSam.Core/Serializers/TableSerializer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using Spectre.Console;
+using Spectre.Console.Rendering;
+
+namespace SierraSam.Core.Serializers;
+
+internal sealed class TableSerializer : ISerializer
+{
+    public IRenderable Serialize<T>(T content)
+    {
+        ArgumentNullException.ThrowIfNull(content, nameof(content));
+
+        var type = typeof(T) switch
+        {
+            { IsGenericType: true } => typeof(T).GetGenericArguments().First(),
+            { IsArray: true } or { IsPointer: true } and { IsByRef: true } => typeof(T).GetElementType()!,
+            _ => typeof(T)
+        };
+
+        var properties = type.GetProperties();
+
+        var table = new Table
+        {
+            Border = TableBorder.Markdown
+        };
+
+        foreach (var prop in properties)
+        {
+            table.AddColumn(prop.Name, column => column.Alignment = Justify.Left);
+        }
+
+        foreach (var obj in content as IEnumerable ?? new [] { content })
+        {
+            var values = new List<string>();
+            foreach (var prop in properties)
+            {
+                var value = prop.GetValue(obj);
+
+                values.Add(value?.ToString() ?? string.Empty);
+            }
+
+            table.AddRow(values.ToArray());
+        }
+
+        return table;
+    }
+}

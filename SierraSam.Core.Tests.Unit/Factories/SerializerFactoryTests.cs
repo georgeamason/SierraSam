e231665233826157a48365cccea840fc1bee69f1
George Mason <george.mason@red-gate.com>
2023-10-10 22:53:33 +0100

    Add serializer factory

---
+++ SierraSam.Core.Tests.Unit/Factories/SerializerFactoryTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using SierraSam.Core.Factories;
+using SierraSam.Core.Serializers;
+
+namespace SierraSam.Core.Tests.Unit.Factories;
+
+internal sealed class SerializerFactoryTests
+{
+    [TestCase("json", typeof(JsonSerializer))]
+    [TestCase("yaml", typeof(YamlSerializer))]
+    [TestCase("none", typeof(EmptySerializer))]
+    public void Factory_creates_correct_implementation_from_configuration(
+        string output,
+        Type implementation)
+    {
+        var configuration = Substitute.For<IConfiguration>();
+        configuration.Output.Returns(output);
+
+        var result = SerializerFactory.Create(configuration);
+
+        result.Should().BeOfType(implementation);
+    }
+}

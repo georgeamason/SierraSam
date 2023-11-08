e2a1fc574024bf14845a7844d8151e443a3f0d1e
George Mason <george.mason@red-gate.com>
2023-11-08 21:11:01 +0000

    Add Oracle IDbContainer implementation

--- SierraSam.Tests.Integration/DbContainerFactory.cs
+++ SierraSam.Tests.Integration/DbContainerFactory.cs
@@ -18,7 +18,8 @@ internal static class DbContainerFactory
         new Postgres("16"),
         new Postgres("15"),
         new MySql("8.2"),
-        new MySql("5.7")
+        new MySql("5.7"),
+        new Oracle("free", "23.3.0.0")
     };
 
     public interface IDbContainer
@@ -27,6 +28,69 @@ internal static class DbContainerFactory
         public Task StartAsync();
         public Task StopAsync();
         public Task Clean();
+    }
+
+    private sealed class Oracle : IDbContainer
+    {
+        private readonly IContainer _container;
+
+        public Oracle(string edition = "free", string tag = "latest")
+        {
+            _container = new ContainerBuilder()
+                .WithImage($"container-registry.oracle.com/database/{edition}:{tag}")
+                .WithPortBinding(1521, true)
+                .WithEnvironment("ORACLE_PWD", Password)
+                .WithEnvironment("ORACLE_SID", "FREE")
+                .WithWaitStrategy(Wait
+                    .ForUnixContainer()
+                    .UntilPortIsAvailable(1521)
+                    .UntilMessageIsLogged("DATABASE IS READY TO USE!")
+                )
+                .Build();
+        }
+
+        public DbConnection DbConnection
+        {
+            get
+            {
+                if (_container.State is not TestcontainersStates.Running)
+                {
+                    throw new Exception("Container must be running to get connection string");
+                }
+
+                var connection = new OdbcConnection(
+                    $"Driver={{Oracle in instantclient_21_12}};" +
+                    $"Dbq=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=127.0.0.1)(PORT={_container.GetMappedPublicPort(1521)}))(CONNECT_DATA=(SERVICE_NAME=FREE)));" +
+                    $"Uid=SYSTEM;Pwd={Password};"
+                );
+
+                connection.Open();
+
+                return connection;
+            }
+        }
+
+        public Task StartAsync() => _container.StartAsync();
+
+        public Task StopAsync() => _container.StopAsync();
+
+        public async Task Clean()
+        {
+            if (DbConnection.State is not ConnectionState.Open)
+            {
+                throw new Exception("Connection must be open to clean database");
+            }
+
+            var respawner = await Respawner.CreateAsync(
+                DbConnection,
+                new RespawnerOptions
+                {
+                    DbAdapter = DbAdapter.Oracle
+                }
+            );
+
+            await respawner.ResetAsync(DbConnection);
+        }
     }
 
     private sealed class SqlServer : IDbContainer

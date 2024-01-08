65dfb5e6eff924ff145ad0c2e061198e28fa451d
George Mason <george.mason@red-gate.com>
2024-01-08 21:11:49 +0000

    Add time provider to Validate.cs

--- SierraSam/Capabilities/Validate.cs
+++ SierraSam/Capabilities/Validate.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using SierraSam.Core.MigrationValidators;
 using Spectre.Console;
 
@@ -8,37 +7,35 @@ namespace SierraSam.Capabilities;
 public sealed class Validate : ICapability
 {
     private readonly ILogger<Validate> _logger;
-    private readonly IMigrationValidator _migrationValidator;
+    private readonly IMigrationValidator _validator;
     private readonly IAnsiConsole _console;
+    private readonly TimeProvider _timeProvider;
 
+    // ReSharper disable once ConvertToPrimaryConstructor
     public Validate(
         ILogger<Validate> logger,
-        IMigrationValidator migrationValidator,
-        IAnsiConsole console)
+        IMigrationValidator validator,
+        IAnsiConsole console,
+        TimeProvider timeProvider
+    )
     {
-        _logger = logger
-            ?? throw new ArgumentNullException(nameof(logger));
-
-        _migrationValidator = migrationValidator
-            ?? throw new ArgumentNullException(nameof(migrationValidator));
-
-        _console = console
-            ?? throw new ArgumentNullException(nameof(console));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _validator = validator ?? throw new ArgumentNullException(nameof(validator));
+        _console = console ?? throw new ArgumentNullException(nameof(console));
+        _timeProvider = timeProvider ?? throw new ArgumentNullException(nameof(timeProvider));
     }
 
     public void Run(string[] args)
     {
         _logger.LogTrace($"{nameof(Validate)} running");
 
-        // if (_database.Connection.State is not ConnectionState.Open) _database.Connection.Open();
-
-        var stopwatch = Stopwatch.StartNew();
-        var validated = _migrationValidator.Validate();
-        stopwatch.Stop();
+        var startTimestamp = _timeProvider.GetTimestamp();
+        var validated = _validator.Validate();
+        var elapsedTime = _timeProvider.GetElapsedTime(startTimestamp);
 
         _console.MarkupLine(
             $"[green]Successfully validated {validated} migrations " +
-            $@"(execution time {stopwatch.Elapsed:mm\:ss\.fff}s)[/]"
+            $@"(execution time {elapsedTime:mm\:ss\.fff}s)[/]"
         );
     }
 }
